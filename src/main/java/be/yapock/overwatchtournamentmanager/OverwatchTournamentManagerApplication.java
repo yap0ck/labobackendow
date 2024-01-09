fee11fd45fab4ef3f57b61afc71e873d005d4f37
Beukens <beukensgaetan@gmail.com>
2024-01-09 09:55:37 +0100

    init

---
+++ src/main/java/be/yapock/overwatchtournamentmanager/OverwatchTournamentManagerApplication.java
@@ -0,0 +1,13 @@
+package be.yapock.overwatchtournamentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverwatchTournamentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverwatchTournamentManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/yapock/overwatchtournamentmanager/OverwatchTournamentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yapock.overwatchtournamentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverwatchTournamentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
